3173a10ff559ee60f66659902805d01d12a1f40d
Vũ Duy <ducduy23899@gmail.com>
2021-12-18 21:56:28 +0700

    vduy add code

--- src/app/navigation/navigation.model.ts
+++ src/app/navigation/navigation.model.ts
@@ -10,30 +10,39 @@ export class FuseNavigationModel implements FuseNavigationModelInterface
         this.model = [
             {
                 'id'      : 'applications',
-                'translate': 'Thông tin',
+                'translate': 'NAV.Personal',
                 'type'    : 'group',
                 'children': [{
                     'id'   : 'user',
                     'translate': 'Thông tin cá nhân',
-                    'icon': 'supervisor_account',
+                    'icon': 'account_circle',
                     'type' : 'item',
                     'url'  : '/information/student-info'
                 },
+                // {
+                //     'id'   : 'menu',
+                //     'translate': 'Thông tin chức năng',
+                //     'type' : 'item',
+                //     'icon': 'apps',
+                //     'url'  : '/information/system-info'
+                // },
                 {
                     'id'   : 'menu',
-                    'translate': 'Thông tin chức năng',
-                    'type' : 'item',
-                    'icon': 'apps',
-                    'url'  : '/information/system-info'
+                    'translate': 'Đăng kí học phần',
+                    'type' : 'playlist_add_check_circle',
+                    'icon': 'item',
+                    'url'  : '/student-manager/subject-subscribe'
                 },
+                
                 {
                     'id'   : 'menu',
                     'translate': 'Học phần đã đăng kí',
                     'type' : 'item',
-                    'icon': 'apps',
+                    'icon': 'playlist_add_check_circle',
                     'url'  : '/information/subject-join'
                 }]
             },
+
             {
                 'id'      : 'applications',
                 'translate': 'NAV.System',
@@ -49,16 +58,17 @@ export class FuseNavigationModel implements FuseNavigationModelInterface
                     'id'   : 'menu',
                     'translate': 'Danh mục học phần',
                     'type' : 'item',
-                    'icon': 'apps',
+                    'icon': 'list',
                     'url'  : '/student-manager/subject-list'
                 },
-                {
-                    'id'   : 'menu',
-                    'translate': 'Đăng kí học phần',
-                    'type' : 'item',
-                    'icon': 'apps',
-                    'url'  : '/student-manager/subject-subscribe'
-                },]
+                // {
+                //     'id'   : 'menu',
+                //     'translate': 'Đăng kí học phần',
+                //     'type' : 'item',
+                //     'icon': 'apps',
+                //     'url'  : '/student-manager/subject-subscribe'
+                // },
+            ]
             },
         ];
     }
